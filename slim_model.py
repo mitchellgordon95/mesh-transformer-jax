190f039c8cd82ae086d0ba59355684b23fe0b2a4
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-07-09 15:39:31 -0400

    Don't assume slim models are coming from GCS

--- slim_model.py
+++ slim_model.py
@@ -17,6 +17,7 @@ from mesh_transformer.util import clip_by_global_norm, to_bf16, to_f16
 def parse_args():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model-dir", type=str, default=None, help="Directory containing model checkpoints.")
     parser.add_argument("--config", type=str, default=None, help="Config file location")
     parser.add_argument("--ckpt-step", type=int, default=-1, help="Step number of the checkpoint to convert (if not specified, converts the most recent checkpoint)")
     parser.add_argument("--f16", default=False, action="store_true", help="Convert to float16 (instead of bfloat16)")
@@ -34,8 +35,7 @@ if __name__ == "__main__":
 
     assert cores_per_replica <= 8
 
-    bucket = params["bucket"]
-    model_dir = params["model_dir"]
+    model_dir = args.model_dir
 
     params["optimizer"] = optax.chain(
         optax.scale(1),
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     mesh_shape = (jax.device_count() // cores_per_replica, cores_per_replica)
     devices = np.array(jax.devices()).reshape(mesh_shape)
 
-    with open(f"gs://{bucket}/{model_dir}/meta.json", "r") as f:
+    with open(f"{model_dir}/meta.json", "r") as f:
         meta = json.load(f)
 
     if args.ckpt_step > -1:
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         network = CausalTransformer(params)
 
         start = time.time()
-        network.state = read_ckpt(network.state, f"gs://{bucket}/{model_dir}/step_{ckpt_step}/", devices.shape[1])
+        network.state = read_ckpt(network.state, f"{model_dir}/step_{ckpt_step}/", devices.shape[1])
         print(f"network loaded in {time.time() - start:.06}s")
 
         start = time.time()
@@ -78,5 +78,5 @@ if __name__ == "__main__":
         suffix = "_slim_f16" if args.f16 else "_slim"
 
         for i in range(cores_per_replica):
-            write_ckpt(network.state, f"gs://{bucket}/{model_dir}{suffix}/step_{ckpt_step}/", i)
+            write_ckpt(network.state, f"{model_dir}{suffix}/step_{ckpt_step}/", i)
             print(f"written shard {i}")
